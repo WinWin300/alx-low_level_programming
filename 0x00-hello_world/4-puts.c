bb3d3845a5077ef26cdccfaafce6de148ec49abb
WinWin300 <atienochristine787@gmail.com>
2022-03-10 01:22:14 -0800

    New Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-char Mystr[] = " Programming is like building a multilingual puzzle ";
+char Mystr[] = "Programming is like building a multilingual puzzle\n";
 puts(Mystr);
 return (0);
 }
