b06e211493e0a50e584573d7d95efa5711279d48
WinWin300 <atienochristine787@gmail.com>
2022-03-10 01:18:19 -0800

    New Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main (void)
-  
-         {
-	   char Mystr[] = " Programming is like building a multilingual puzzle ";
-
-	   puts(Mystr);
-
-	   return (0);
-	   
-	 }
+/**
+*main -prints to screen
+*Return: zero
+*/
+int main(void)
+{
+char Mystr[] = " Programming is like building a multilingual puzzle ";
+puts(Mystr);
+return (0);
+}
