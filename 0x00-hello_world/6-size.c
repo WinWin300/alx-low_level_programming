62fd929c054a6bb61de336dedd0a13e699b5e6f4
WinWin300 <atienochristine787@gmail.com>
2022-03-10 09:47:08 -0800

    New Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,10 @@
  */
 int main(void)
 {
-char c;
-int i;
-float f;
-printf("Size of char: %lu byte\(s)\n", sizeof(c));
-printf("Size of int: %lu byte\(s)\n", sizeof(i));
-printf("Size of long int: %lu byte\(s)\n", sizeof(long int));
-printf("Size of long long int: %lu byte\(s)\n", sizeof(long long int));
-printf("Size of float: %lu byte\(s)\n", sizeof(f));
+printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of int: %ld byte(s)\n", sizeof(int));
+printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
