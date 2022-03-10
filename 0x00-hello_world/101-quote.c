e0ee2156e5985329a1585bdec6c65751f2e14b93
WinWin300 <atienochristine787@gmail.com>
2022-03-10 03:35:57 -0800

    New Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main - prints to the standard error
  *Return: one
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
 return (1);
 }
