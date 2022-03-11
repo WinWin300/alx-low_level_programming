899f6e4fec6eb1d81ff15b6d01c49622fe847086
WinWin300 <atienochristine787@gmail.com>
2022-03-11 13:55:46 -0800

    This is getting a bit challenging

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main -Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 0;
+int y;
+while (x <= 9)
+{
+y = 0;
+while (y <= 9)
+{
+if (x != y && x < y)
+{
+putchar(x + 48);
+putchar(y + 48);
+if (x + y != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+++y;
+}
+++x;
+)
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 0;
+int y;
+while (x <= 99)
+{
+y = x;
+while (y <= 99)
+{
+if (y != x)
+{
+putchar((x / 10) + 48);
+putchar((x % 10) + 48);
+putchar(' ');
+putchar((y / 10) + 48);
+putchar((y % 10) + 48);
+if (x != 98 || y != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+++y;
+}
+++x;
+}
+putchar('\n');
+return (0);
+}
