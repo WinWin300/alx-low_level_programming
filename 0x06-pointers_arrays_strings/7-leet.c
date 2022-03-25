93f3dd6c7aaa1e05531ed340744c140abad0ede1
Christine Atieno <50162963+WinWin300@users.noreply.github.com>
2022-03-25 12:40:25 +0300

    Update 7-leet.c
    New commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,37 @@
-#include "main.h"
-
 /**
- * *leet - check the code for Holberton School students.
+ * leet - a function that encodes a string into 1337
  *
- * @str: string.
- * Return: string.
- */
-char *leet(char *str)
-{
-	int i;
+ * @s: string input
+ *
+ * Return: @s
+*/
 
-	for (i = 0; str[i] != '\0'; i++)
+char *leet(char *s)
+{
+	int i, c = 0;
+	int sl[] = {97, 101, 111, 116, 108};
+	int ul[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
+
+	/*iterate values in array s*/
+	while (s[c] != '\0')
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		/**
+		 * loop through array value five times
+		 * to check if the value is equal to
+		 * any small letter in array sl or
+		 * upper letter in array ul and if it
+		 * is replace it with the value in array n
+		*/
+		for (i = 0; i < 5; i++)
 		{
-			str[i] = '4';
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
 		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
+		c++;
 	}
-	return (str);
+	return (s);
 }
