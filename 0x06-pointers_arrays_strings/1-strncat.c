9d28ed2e973cbb9cb794f7f0a2525e88bdbe88f5
Christine Atieno <50162963+WinWin300@users.noreply.github.com>
2022-03-25 12:06:23 +0300

    Update 1-strncat.c
    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,38 @@
-**
- * _strncat - a function that concatenates two strings.
- *
- * @dest: pointer to destination input
- * @src: pointer to source input
- * @n: most number of bytes from @src
- *
- * Return: @dest
-*/
+#include "main.h"
 
+/**
+ * *_strncat - concatenates two strings.
+ *
+ * @dest: resulting string.
+ * @src: source string.
+ * @n: string length.
+ * Return: pointer to dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int i, m;
 
-	c = 0;
-
-	/*find size of dest array*/
-	while (dest[c])
-		c++;
-
-	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
-	*/
+	m = string_length(dest);
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/*null terminate dest*/
-	dest[c + i] = '\0';
-
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
 	return (dest);
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
