3a95f3cd783f0e2e2bf418ede4730c47034bd79f
Christine Atieno <50162963+WinWin300@users.noreply.github.com>
2022-03-28 09:28:20 +0300

    Update main.h
    commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,15 +17,15 @@ char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
 
-/*my header*/
-int _putchar(char c);
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s);
 void puts2(char *str);
+void rev_string(char *s);
+int string_length(char*);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
